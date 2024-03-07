4e868dfe8a95c78b88dba87a5a70eef4e26a8480
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-07 23:12:11 +0530

    Terminal Counter Done

---
+++ JavaScript/Problems/terminalClock.js
@@ -0,0 +1,19 @@
+let intervalId
+
+function showTime() {
+    const d  = new Date()
+    let hours =  d.getHours()
+    let minutes =  d.getMinutes()
+    let seconds =  d.getSeconds()
+    hours = (hours<10)? ("0"+hours):hours
+    minutes = (minutes<10)? ("0"+minutes):minutes
+    seconds = (seconds<10)? ("0"+seconds):seconds
+    let time = hours+" : "+minutes+" : "+seconds
+    process.stdout.write(time)
+    intervalId = setInterval(showTime, 1000)
+    process.stdout.cursorTo(0)
+
+}
+
+
+showTime()
