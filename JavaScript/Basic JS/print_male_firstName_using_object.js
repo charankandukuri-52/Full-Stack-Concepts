97ba3d97ac44da66b1fc563c4905602f80939de2
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-07 18:41:25 +0530

    Objects

--- JavaScript/Basic JS/print_male_firstName_using_object.js
+++ JavaScript/Basic JS/print_male_firstName_using_object.js
@@ -1,5 +1,7 @@
 
 //  Creating an Array of Objects
+
+// we have an array called persons that contains multiple objects, each representing a person. Each object has two properties: firstName and gender, with corresponding values for each person.
 let persons = [
     {
         firstName:'charan',
@@ -14,8 +16,11 @@ let persons = [
         gender:'Female'
     }
 ]
+// Now, we have a for loop that iterates over each object in the persons array. It starts at index 0 and continues until the index is less than the length of the persons array.
 for(let i=0; i<persons.length; i++) {
     if(persons[i]["gender"] === 'Male') {
         console.log(persons[i]['firstName'])
     }
 }
+
+// Within the loop, there's an if statement that checks if the gender property of the current person object is equal to 'Male'. If it is, then it prints the firstName of that person to the console.
