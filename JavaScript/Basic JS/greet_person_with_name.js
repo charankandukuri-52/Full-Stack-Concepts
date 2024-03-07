c2488bdf2ed562773749cc4d2bff6f23d39df8a4
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-07 18:35:56 +0530

    Print Statement

--- JavaScript/Basic JS/greet_person_with_name.js
+++ JavaScript/Basic JS/greet_person_with_name.js
@@ -1,3 +1,11 @@
 let first_name = "Hello" 
 let last_name = 'people'
 console.log('Hi' + first_name + last_name)
+
+/*
+let first_name = "Hello";: Here, we're creating a variable named first_name using the let keyword. Variables are like containers that hold information. We're assigning the value "Hello" to this variable. The value is a string, indicated by the double quotes " " around it. Strings are sequences of characters, like words or sentences.
+
+let last_name = 'people';: This line is similar to the previous one, but here we're creating a variable named last_name and assigning it the value 'people'. Again, it's a string, but this time we're using single quotes ' ' to indicate that it's a string. In JavaScript, you can use either double quotes or single quotes to create strings.
+
+console.log('Hi' + first_name + last_name);: Here, we're using the console.log() function to output a message to the console. The message consists of the string 'Hi', followed by the value of the first_name variable, and then the value of the last_name variable. We use the + operator to concatenate (combine) the strings together. So, when this line of code runs, it will print "HiHellopeople" to the console.
+*/
