8a9b2a31d0b88679cc40cbe0bef32f7d2b5a4e04
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-07 18:15:26 +0530

    Basic Array Problem added

---
+++ JavaScript/Basic JS/print_even_numbers_in_array.js
@@ -0,0 +1,13 @@
+let working_array = []
+// Adding numbers from 1 to 1000 in array
+for(let pointer = 1; pointer <= 1000; pointer++) {
+    working_array.push(pointer)
+}
+//  Now Working Array has elements from 1 to 1000 
+for(let pointer = 0;pointer<working_array.length;++pointer) {
+    // Accessing elements in array
+    // General Syntax is array_name[index] => value present in that index of the array
+    if(working_array[pointer] % 2 === 0) {
+        console.log(working_array[pointer])
+    }
+}
